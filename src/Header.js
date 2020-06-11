d59b824d10d4f8cb9024e4d87a31cb425053d85a
Tijan Jackson <57716770+TijanJackson@users.noreply.github.com>
2020-06-11 13:12:57 +0200

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+
+function Header() {
+    return (
+        <div>
+            <h1>React Calculator</h1>
+        </div>
+    )
+}
+
+export default Header;
